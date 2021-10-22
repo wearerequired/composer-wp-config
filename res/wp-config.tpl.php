7356b6d2b6656c37adb0aa7638bc5296a1a6fada
Ulrich Pogson <grapplerulrich@gmail.com>
2021-10-22 08:18:02 +0200

    Use spaces not tabs for mid-line alignment

--- res/wp-config.tpl.php
+++ res/wp-config.tpl.php
@@ -42,7 +42,7 @@ global $table_prefix;
  * Environment variables.
  *
  * - Load from `.env`.
- * 	- If environment variables are set, create an array. Happens when wp-config.php is loaded again.
+ *  - If environment variables are set, create an array. Happens when wp-config.php is loaded again.
  *  - If environment variables are not set, set as environment variables.
  * - Check for required variables.
  * - Define constant for each variable if not already defined.
