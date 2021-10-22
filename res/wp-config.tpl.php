4dc2e6593ebfa366a33b04d16f30fe47b70aba77
Ulrich Pogson <grapplerulrich@gmail.com>
2021-10-22 17:00:02 +0200

    Use createArrayBacked() as fallback when already loaded (#31)
    This is an issue when `WP_CLI::runcommand()` is used causing `wp-config.php` to be loaded a second time. Due to the environment variables already existing `Dotenv::createUnsafeImmutable()` returns an empty array causing the constants not to be set. `Dotenv::createArrayBacked()` is used as a fallback.

--- res/wp-config.tpl.php
+++ res/wp-config.tpl.php
@@ -42,10 +42,17 @@ global $table_prefix;
  * Environment variables.
  *
  * - Load from `.env`.
+ *  - If environment variables are not set, set as environment variables.
+ *  - If environment variables are set, create an array. Happens when wp-config.php is loaded again.
  * - Check for required variables.
  * - Define constant for each variable if not already defined.
  */
 $dotenv    = Dotenv::createUnsafeImmutable( ___WP_CONFIG_ENV_PATHS___ );
+$variables = $dotenv->load();
+if ( empty( $variables ) ) {
+	$dotenv    = Dotenv::createArrayBacked( ___WP_CONFIG_ENV_PATHS___ );
+	$variables = $dotenv->load();
+}
 $variables = $dotenv->load();
 $dotenv->required(
 	[
